42f00a3f7aa23fda9c5a5c4db787efc2883043ef
roshanthamalaka <roshanthamalaka@gmail.com>
2025-06-03 21:31:19 +0530

    Working Code Upto Showing Gandalf image

---
+++ Readme.md
@@ -0,0 +1,8 @@
+References
+Creating Python Web App using Flask:
+https://medium.com/@dattu1993/creating-a-web-application-with-python-a-comprehensive-guide-for-beginners-db59df5867e4
+
+Flask Documentation
+https://flask.palletsprojects.com/en/stable/quickstart/
+
+There several frameworks available with Python for Web Applications. Popular framework are Flask and Django. I have used because it is lightweight.
---
+++ app.py
@@ -0,0 +1,29 @@
+
+from flask import Flask,render_template
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+
+# When App loads it shows Welcome to App Screen
+# Use route() decorator to tell Flask what URL should trigger our function.
+@app.route("/")
+def defaultpage():
+    return "Welcome to My App"
+
+# To show the Gandalf image had to use Rendering Template in Flask 
+# Documentation https://flask.palletsprojects.com/en/stable/quickstart/#rendering-templates 
+# Need to create templates folder under that need to create an html file shows the image. 
+@app.route("/gandalf")
+def gandalfpage():
+ return  render_template('gandalf.html')
+
+# To print time we have to use datetime module and its function now
+# https://docs.python.org/3/library/datetime.html
+
+@app.route("/colombo")
+def timePage():
+    IST = pytz.timezone('Asia/Colombo')
+    colombo = datetime.now(IST)
+    print ("Current Time is {colombo}")
+    return "Current Time is {colombo}"
