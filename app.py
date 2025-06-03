ca9b1bf537b75fc4b0ea27bb329302278f3a84b9
roshanthamalaka <roshanthamalaka@gmail.com>
2025-06-03 22:01:20 +0530

    Python App with Functonality of Display Gandalf image,Display Colomb Time and Display Customized error message for invalida URL paths

--- app.py
+++ app.py
@@ -20,10 +20,19 @@ def gandalfpage():
 
 # To print time we have to use datetime module and its function now
 # https://docs.python.org/3/library/datetime.html
+# Get Time for Time Zone  https://www.geeksforgeeks.org/get-current-time-in-different-timezone-using-python/
+# Formating the Time from datetime using strftime https://www.programiz.com/python-programming/datetime/strftime
+
 
 @app.route("/colombo")
 def timePage():
     IST = pytz.timezone('Asia/Colombo')
-    colombo = datetime.now(IST)
-    print ("Current Time is {colombo}")
-    return "Current Time is {colombo}"
+    colombo_datetime = datetime.now(IST) # This will return time in Full format  2025-06-03 21:36:48.742131+05:30
+    time_now = colombo_datetime.strftime("%H:%M:%S") # Since Need only Current as per the requirment, So format the using strftime function
+    print ("Colombo Time is ",time_now) # Added as Debugging step to see the time as inted format 
+    return "Colombo Time when the Request Made: "+ time_now
+
+# Error Handling with Error message
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html'), 404
